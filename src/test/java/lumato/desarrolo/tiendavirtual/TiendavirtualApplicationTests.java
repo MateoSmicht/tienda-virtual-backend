27113e12ffdb8d3b3d9b56d20305e03f8131c024
Mateo Smicht <mateosmicht13@gmail.com>
2026-04-07 20:56:15 -0300

    Actualizacion del sistema.

--- src/test/java/lumato/desarrolo/tiendavirtual/TiendavirtualApplicationTests.java
+++ src/test/java/lumato/desarrolo/tiendavirtual/TiendavirtualApplicationTests.java
@@ -1,9 +1,11 @@
 package lumato.desarrolo.tiendavirtual;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 class TiendavirtualApplicationTests {
 
 	@Test
